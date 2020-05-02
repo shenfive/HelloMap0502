992ee0212b36d24acf1e5d77bcf46dc3d9843f34
Danny Shen <shenfive@gmail.com>
2020-05-02 13:06:45 +0800

    Initial Commit

---
+++ HelloMap0502/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMap0502
+//
+//  Created by 申潤五 on 2020/5/2.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
